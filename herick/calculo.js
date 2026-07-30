22d4ec7ebf10bc9ebad62fa88a5a3b3651447370
herickmineli-source <herick.mineli@escola.pr.gov.br>
2026-07-30 11:46:26 -0300

    Add files via upload

---
+++ herick/calculo.js
@@ -0,0 +1,11 @@
+alert("olá feio! seja bem vindo a calculadora.");
+let numero1 = parseInt  (0); // variavel que vai receber o numero1
+let numero2 = parseInt (0);// variavel que vai receber o numero2
+let resultado = parseInt (0);// variavel que vai mnostrar o resultado
+
+numero1  = 35;// variavel numero1 recebe primeiro valor
+numero2 = 32;// variavel numero2 recebe o segundo valor
+numero1 = parseInt(prompt("digite o primeiro numero:"))
+numero2 = parseInt(prompt("digite o segundo numero:"))
+resultado =numero1 + numero2// variavel resultado receb
+alert(resultado);//variavel resultado é mostrada na tela 
